517fb30f10b750df10fe627c4b6d42f4abda45b5
Nehmulos <nehmu@lavabit.com>
2013-07-03 00:01:49 +0200

    added file I forgot to add earlier;

---
+++ GameMode.rs
@@ -0,0 +1,25 @@
+
+trait GameMode {
+    pub fn new() -> Self;
+    pub fn update(&self,dt:int);
+    pub fn draw(&self);
+}
+
+pub struct SpaceMode {
+    score:int
+}
+
+//impl GameMode  for SpaceMode {
+// Either I'm stupid or it's this bug: https://github.com/mozilla/rust/issues/4100
+impl SpaceMode {
+    pub fn new() -> SpaceMode {
+        SpaceMode {score:0}
+    }
+    pub fn update(&self, dt:int) {
+        
+    }
+    pub fn draw(&self) {
+        println("you are in space");
+    }
+}
+
