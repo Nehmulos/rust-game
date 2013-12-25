0690b949a55425108c0ca5598584bb80ac1088d2
Nehmulos <nehmu@lavabit.com>
2013-12-25 01:12:12 +0100

    open a window;

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,5 +1,7 @@
 extern mod rsfml;
-//use sdl2;
+
+use rsfml::window::{Window, VideoMode, ContextSettings};
+use rsfml::graphics::{RenderWindow, sfDefaultStyle};
 
 use self::texturecache::TextureCache;
 use self::gamemode::{SpaceMode};
@@ -10,14 +12,20 @@ mod gamemode;
 pub struct Engine {
     textureCache:~TextureCache,
     mode:~SpaceMode,
+    window:~RenderWindow,
 }
 
 impl Engine {
     pub fn new() -> Engine {
+        let window = match(RenderWindow::new(VideoMode::new_init(800, 600, 24), "bst gaem 4eva", sfDefaultStyle, &ContextSettings::default())) {
+            Some(v) => ~v,
+            None() => fail!("fugg no window DDD:")
+        };
         return Engine {
             textureCache: ~TextureCache::new(),
             mode: ~SpaceMode::new(),
-        }
+            window: window
+        };
     }
 
     pub fn init(&self) {
